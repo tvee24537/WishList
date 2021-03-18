ff9cc12a77f91bf24cc1158c6a08bdd03da5ffe5
Thanapat <t.veerakajorn@gmail.com>
2021-03-18 18:38:56 -0400

    updated destinationShow

---
+++ wish_list_client/src/components/DestinationShow.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Card, CardBody, CardTitle, CardImg, Button } from 'reactstrap';
+import Activities from '../containers/Activities';
+import { deleteDestination, toggleGranted, toggleWaitList } from '../actions/destinationActions';
+
+
+const DestinationShow = props => {
+
+  let destination = props.destinations.filter(destination => destination.id == props.match.params.id)[0];
+
+  const handleDelete = () => {
+    props.deleteDestination(destination.id);
+    const path = `/destinations`;
+    props.history.push(path);
+  };
+
+  const handleGranted = () => {
+    props.toggleGranted(destination, destination.id)
+  }
+
+  const handleWaitList = () => {
+    props.toggleWaitList(destination, destination.id)
+  }
+
+  return (
+    <div>
+      <Card className = 'DestinationShow'>
+      <CardBody>
+          <CardTitle style={{border: 'dotted' }}>{destination && destination.name} - {destination && destination.country}</CardTitle>
+          <CardImg className='DestinationImage' src={destination && destination.image} alt={destination && destination.name} /><br></br><br></br>
+        </CardBody>
+        <Button onClick={handleGranted} className='GrantedButton' style={{alignItems: 'center' }}>
+          {destination && destination.granted === false ? 'Add to Granted' : 'You Have Granted'}
+        </Button>
+        <Button onClick={handleWaitList} className='WaitListButton'>
+          {destination && destination.wait_list === false ? 'Add to Wait List' : 'This Is In your Wait List'}
+        </Button>
+        <Button onClick={handleDelete} className='DeleteButton'>Delete This Destination</Button>
+      </Card>
+
+      <Activities destination={destination} />
+    </div>
+  );
+};
+
+export default connect(null, { deleteDestination, toggleGranted, toggleWaitList })(DestinationShow);
