d660a630a56c5514738f05bc77a27aa2a8ad2b8c
Thanapat <t.veerakajorn@gmail.com>
2021-03-18 18:23:41 -0400

    updated destinationCard

---
+++ wish_list_client/src/components/DestinationCard.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Card, CardBody, CardTitle, CardImg, Button } from 'reactstrap';
+
+class DestinationCard extends React.Component {
+
+    callApi = () => {
+        console.log('a')
+        fetch(`http://localhost:3000/destinations/1`)
+          .then(res => {
+            console.log(res)
+            if (!res.ok) {
+              throw new Error(res.statusText)
+            } else {
+            console.log('b')
+            return res.json()
+            }
+        })
+        .then(destination => console.log('c', destination))
+        .catch(error => console.log('d', error))
+        console.log('e')
+
+      // 'a', 'e', 'd'
+    }
+
+    render () {
+        let {destination} = this.props
+        return (
+            <Card className = 'DestinationCard'>
+            <CardBody>
+                <CardTitle>{destination.name} - {destination.country}</CardTitle>
+                <CardImg className='DestinationImage' src={destination.image} alt={destination.name} />
+                <Button onClick={this.callApi}>Call API</Button>
+            </CardBody>
+            </Card>
+        )
+    }
+
+}
+
+export default DestinationCard;
