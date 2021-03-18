abac73f0d0c919a43d7553dcd951d4c9f0b29f96
Thanapat <t.veerakajorn@gmail.com>
2021-03-18 19:44:25 -0400

    updated giftList

---
+++ wish_list_client/src/components/GiftsList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { deleteGift } from '../actions/giftActions';
+
+// adding/deleting gifts from destinations
+const GiftsList = props => {
+
+  const handleDelete = (gift) => {
+    props.deleteGift(gift.id, gift.destination_id)
+  }
+
+  return (
+    <div className='GiftsList'>
+      {props.gifts && props.gifts.length === 0 ? <h6>There aren't any gifts for this person yet</h6> : <h5>Below are some example gift ideas for this person.</h5>}
+      <ListGroup>
+        {props.gifts && props.gifts.map(gift =>
+          <ListGroupItem style={{fontSize: 'xx-large' }} key={gift.id}>{gift.title}   <Button style={{backgroundColor: 'black'}}onClick={() => handleDelete(gift)} className='DeleteButton'>Delete This Gift</Button></ListGroupItem>
+        )}
+      </ListGroup>
+    </div>
+  );
+};
+
+export default connect(null, { deleteGift })(GiftsList);
