116b63c52057a2ce2880f1fd0a2095a447deddfc
Thanapat <t.veerakajorn@gmail.com>
2021-03-18 15:24:41 -0400

    updating imports and reactDOM.render

--- wish_list_client/src/index.js
+++ wish_list_client/src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import destinationReducer from './reducers/destinationReducer';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './App.css';
 
+
+// Create store
+// Use applyMiddleware to enable thunk
+let store = createStore(destinationReducer, composeEnhancers(applyMiddleware(thunk)));
+
+// Wrap entire app in provider to give all components access to the store
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store = { store }>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
